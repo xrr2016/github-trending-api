be24db72c54dd014b57f09970b30ed0d6c11acdc
轻剑快马 <xiaoranran1993@outlook.com>
2019-03-15 04:28:44 +0800

    fix style

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -29,14 +29,6 @@ export class AboutComponent implements OnInit {
 }
 `
 
-  data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.'
-  ]
-
   constructor() {}
 
   ngOnInit() {
